467f56c44d6fae9d8a63b005bd638d09f682024c
Diego Gaspar <cadastro@diegogaspar.dev.br>
2025-02-23 15:30:45 -0300

    feat: view home, products and where we are

--- app/Views/products.php
+++ app/Views/products.php
@@ -27,7 +27,58 @@
         </div>
     </nav>
 
-    <h1>Produtos</h1>
+    <!-- main -->
+    <section class="container">
+        <div class="col">
+            <!-- burger 01 -->
+            <div class="row mb-5 product-box">
+                <div class="col-5 text-center">
+                    <img class="img-fluid" src="<?= base_url('assets/images/burger_01.png') ?>" alt="Burger 1" />
+                </div>
+                <div class="col-7 p-5">
+                    <h1 class="mb-3 product-text-color">Burguer 1</h1>
+                    <p class="mb-3">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+                    <h2 class="mt-3 product-text-color">R$15,00</h2>
+                </div>
+            </div>
+
+            <!-- burger 02 -->
+            <div class="row mb-5 product-box">
+                <div class="col-7 p-5">
+                    <h1 class="mb-3 product-text-color">Burguer 2</h1>
+                    <p class="mb-3">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+                    <h2 class="mt-3 product-text-color">R$14,00</h2>
+                </div>
+                <div class="col-5 text-center">
+                    <img class="img-fluid" src="<?= base_url('assets/images/burger_02.png') ?>" alt="Burger 2" />
+                </div>
+            </div>
+
+            <!-- burger 03 -->
+            <div class="row mb-5 product-box">
+                <div class="col-5 text-center">
+                    <img class="img-fluid" src="<?= base_url('assets/images/burger_03.png') ?>" alt="Burger 3" />
+                </div>
+                <div class="col-7 p-5">
+                    <h1 class="mb-3 product-text-color">Burguer 3</h1>
+                    <p class="mb-3">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+                    <h2 class="mt-3 product-text-color">R$12,00</h2>
+                </div>
+            </div>
+
+            <!-- burger 04 -->
+            <div class="row mb-5 product-box">
+                <div class="col-7 p-5">
+                    <h1 class="mb-3 product-text-color">Burguer 4</h1>
+                    <p class="mb-3">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+                    <h2 class="mt-3 product-text-color">R$9,00</h2>
+                </div>
+                <div class="col-5 text-center">
+                    <img class="img-fluid" src="<?= base_url('assets/images/burger_04.png') ?>" alt="Burger 4" />
+                </div>
+            </div>
+        </div>
+    </section>
 
     <!-- social networks -->
     <footer class="container-fluid mt-5">
--- app/Views/where_we_are.php
+++ app/Views/where_we_are.php
@@ -27,7 +27,42 @@
         </div>
     </nav>
 
-    <h1>Onde estamos?</h1>
+    <!-- main -->
+    <section class="container product-box py-5">
+        <div class="row">
+            <div class="col-5 text-center">
+                <img class="img-fluid" src="<?= base_url('assets/images/room.jpg') ?>" alt="Our room">
+            </div>
+            <div class="col-6">
+                <p class="where-we-are-title mb-0">CIGBurger Paris</p>
+                <p class="where-we-are-subtitle">Rua des hamburgers, 123 Paris</p>
+                <p class="mb-3">Ao contrário do que se acredita, Lorem Ipsum não é simplesmente um texto randômico. Com mais de 2000 anos.</p>
+
+                <div class="d-flex align-items-center mb-3">
+                    <img src="<?= base_url('assets/images/icon_phone.png') ?>" alt="Phone" />
+                    <p class="where-we-are-subtitle ms-3">
+                        <a class="nav-link" href="tel:+5532998274714">+55 (32) 9 9827-4714</a>
+                    </p>
+                </div>
+
+                <div class="d-flex align-items-center mb-3">
+                    <img src="<?= base_url('assets/images/icon_email.png') ?>" alt="Email" />
+                    <p class="where-we-are-subtitle ms-3">
+                        <a class="nav-link" href="mailto:contato@cigburger.com.br">contato@cigburger.com.br</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- map -->
+    <section class="container product-box py-5">
+        <div class="row">
+            <div class="col text-center">
+                <img src="<?= base_url('assets/images/map.jpg') ?>" alt="Map" />
+            </div>
+        </div>
+    </section>
 
     <!-- social networks -->
     <footer class="container-fluid mt-5">
