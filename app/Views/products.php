3be9872bd501ea9222eb39c937d0d7dc6b4c94f2
Diego Gaspar <cadastro@diegogaspar.dev.br>
2025-02-23 00:11:23 -0300

    feat: start project

---
+++ app/Views/products.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CIG-Burger</title>
+    <!-- bootstrap -->
+    <link rel="shortcut icon" href="<?= base_url('assets/images/logo.png') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/app.css') ?>">
+</head>
+<body>
+    <!-- nav -->
+    <nav class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col p-3">
+                <!-- logo -->
+                <img src="<?= base_url('assets/images/logo.png') ?>" alt="CIGBurger Logo">
+            </div>
+            <div class="col p-3 pe-5 d-flex flex-row justify-content-end">
+                <div><a class="nav-link ms-5" href="<?= site_url('/') ?>">Início</a></div>
+                <div><a class="nav-link ms-5" href="<?= site_url('products') ?>">Produtos</a></div>
+                <div><a class="nav-link ms-5" href="<?= site_url('where_we_are') ?>">Onde estamos</a></div>
+            </div>
+        </div>
+    </nav>
+
+    <h1>Produtos</h1>
+
+    <!-- social networks -->
+    <footer class="container-fluid mt-5">
+        <div class="row justify-content-center">
+            <div class="col-6 d-flex flex-row justify-content-center">
+                <div class="text-center mx-4">
+                    <a href="#">
+                        <img src="<?= base_url('assets/images/facebook.png') ?>" alt="Facebook" />
+                    </a>
+                </div>
+                <div class="text-center mx-4">
+                    <a href="#">
+                        <img src="<?= base_url('assets/images/instagram.png') ?>" alt="Instagram" />
+                    </a>
+                </div>
+                <div class="text-center mx-4">
+                    <a href="#">
+                        <img src="<?= base_url('assets/images/whatsapp.png') ?>" alt="Whatsapp" />
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col text-center">
+                <h6>Todos os direitos reservados &copy; <?= date('Y') ?></h6>
+            </div>
+        </div>
+    </footer>
+
+    <!-- bootstrap -->
+    <script src="<?= base_url('assets/bootstrap/bootstrap.bundle.min.js') ?>"></script>
+</body>
+</html>
